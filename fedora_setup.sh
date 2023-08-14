bd3b4fbba4f321071fe37c3b71ab8a4f15821e1a
Joshua Ferguson <joshua.ferguson.273@gmail.com>
2023-08-14 14:41:54 -0500

    will figure out how to set selinux context without copying later

--- fedora_setup.sh
+++ fedora_setup.sh
@@ -3,10 +3,12 @@
 sudo cp swayrun /usr/libexec/swayrun
 sudo cp swayrun.desktop /usr/share/wayland-sessions/swayrun.desktop
 
+# system_u:object_r:usr_t:s0
 sudo semanage fcontext -a -e /usr/share/wayland-sessions/sway.desktop /usr/share/wayland-sessions/swayrun.desktop
 sudo restorecon /usr/share/wayland-sessions/swayrun.desktop
 
 # the below upowerd should be installed by default, if not change to another
 # libexec binary
-sudo semanage fcontext -a -e /usr/libexec/upowerd /usr/libexec/swayrun
+# system_u:object_r:bin_t:s0
+sudo semanage fcontext -a -e /usr/libexec/xdg-desktop-portal /usr/libexec/swayrun
 sudo restorecon -vvRF /usr/libexec/swayrun
